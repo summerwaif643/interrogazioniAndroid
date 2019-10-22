6d1d3f481ff9f678c06a30db66b7f3c886298bf1
DDave <davideditrocchio.ddt@gmail.com>
2019-10-22 11:07:30 +0200

    Ver 1.2 Added Notification support, reduced randoms to 2

--- app/src/main/java/com/example/interrogazioni/MainActivity.java
+++ app/src/main/java/com/example/interrogazioni/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.interrogazioni;
 
 import android.content.SharedPreferences;
 import android.os.PersistableBundle;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -27,19 +28,38 @@ public class MainActivity extends AppCompatActivity {
         TextView nInterrogato = (TextView) findViewById(R.id.numberToInterrogate);
         SharedPreferences prefs = getSharedPreferences("com.example.", MODE_PRIVATE);
 
-        int n = 0;
-        String stringN;
-        n = interrogato.nextInt(19) + 1;
-
-        stringN = Integer.toString(n);
-        nInterrogato.setText(stringN);
-
-        interrogato.nextInt(19);
-
         RandomOrgClient client = RandomOrgClient.getRandomOrgClient(apiKey);
 
-        client.generateIntegers(19, 1, 19);
+        try{
+            NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "C:\\Users\\xDave\\AndroidStudioProjects\\interrogazioni\\app\\src\\main\\res\\drawable\\WPVG_icon_2016.svg.png")
+                    .setContentTitle("Random")
+                    .setContentText("Sono stati generati dei numeri")
+                    .setPriority(NotificationCompat.PRIORITY_MAX);
+
+            int[] numbers = client.generateIntegers(2, 1, 19);
+
+            for(int i=0; i<numbers.length; i++){
+                nInterrogato.append(Integer.toString(numbers[i]));
+                nInterrogato.append(", ");
+            }
+
+            for(int i=0; i<numbers.length; i++){
+                numbers[i] = 0;
+            }
+        }
+
+        catch(java.net.MalformedURLException e){
+                e.printStackTrace();
+        }
+        catch(java.io.IOException ex){
+                ex.printStackTrace();
+        }
 
 
+    }
+
+    public void interrogatoRst(View view) {
+        TextView nInterrogato = (TextView) findViewById(R.id.numberToInterrogate);
+        nInterrogato.setText("");
     }
 }
